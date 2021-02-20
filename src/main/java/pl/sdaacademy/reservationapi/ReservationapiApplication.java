cdd01fbf041b2d5b1134e3d5d29867c9198afa4a
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2021-02-20 13:06:39 +0100

    init(crrs): init project

---
+++ src/main/java/pl/sdaacademy/reservationapi/ReservationapiApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.reservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdaacademy/reservationapi/ReservationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.reservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
