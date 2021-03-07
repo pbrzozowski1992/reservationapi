a816a89c941a91554d80eee054cf908e092ebd1b
Piotr Brzozowski <piotr.brzozowski.dev@gmail.com>
2021-03-07 12:35:17 +0100

    fix(crrs): swagger configuration

--- src/main/java/pl/sdaacademy/reservationapi/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/sdaacademy/reservationapi/configuration/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -58,6 +59,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new AuthenticationFilter(authenticationManager(), authKey, authType, signInKey, tokenExpirationTime))
                 .addFilter(new AuthorizationFilter(authenticationManager(), authKey, authType, signInKey));
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
     }
 
